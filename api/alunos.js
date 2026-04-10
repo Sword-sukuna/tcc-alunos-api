ab2787ac3cd1026c787115c03cb69241798187c6
Raul Rosa <109114162+Sword-sukuna@users.noreply.github.com>
2026-04-10 16:08:32 -0300

    .

--- api/alunos.js
+++ api/alunos.js
@@ -6,22 +6,12 @@ export default async function handler(req, res) {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
 
-  // cadastrar aluno
-  if (req.method === "POST") {
-    const { nome, idade, email, curso } = req.body;
-
-    await db.execute(
-      "INSERT INTO alunos (nome, idade, email, curso) VALUES (?, ?, ?, ?)",
-      [nome, idade, email, curso]
-    );
-
-    return res.status(200).json({ message: "Aluno cadastrado" });
-  }
-
-  // buscar alunos
   const [rows] = await db.execute("SELECT * FROM alunos");
   res.status(200).json(rows);
 }
